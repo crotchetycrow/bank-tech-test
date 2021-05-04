010c0baf8f693117132b98c2bb17433e81bf505c
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2021-05-04 18:08:49 +0100

    added balance test for transactions

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -2,11 +2,12 @@ require 'date'
 
 class Transactions
     
-  attr_reader :date, :credit
+  attr_reader :date, :credit, :balance
 
-  def initialize(date, credit)
+  def initialize(date, credit, balance)
     @date = Date.today
     @credit = credit
+    @balance = balance
   end
 
 end
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -2,12 +2,15 @@ require './lib/transactions'
 require 'date'
 
 describe Transactions do
-  let(:test_tran) { Transactions.new(Date.today, 1000) }
+  let(:test_tran) { Transactions.new(Date.today, 1000, 10) }
 
   it 'prints a date' do
     expect(test_tran.date).to eq(Date.today)
   end
   it 'prints deposit/withdraw' do
     expect(test_tran.credit).to eq(1000)
+  end
+  it 'prints balance' do
+    expect(test_tran.balance).to eq(10)
   end
 end
